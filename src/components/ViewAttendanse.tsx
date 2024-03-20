bcbcac7822c8cb899a623a15b680683a4a4a423e
Puskar-Roy <puskarroy600@gmail.com>
2024-03-21 02:52:06 +0530

    feat : attendance list

--- src/components/ViewAttendanse.tsx
+++ src/components/ViewAttendanse.tsx
@@ -3,8 +3,17 @@ import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
 
+interface Attendance {
+  id: number;
+  userName: string;
+  status: string;
+  email: string;
+  date: string;
+}
+
 const ViewAttendanse: React.FC = () => {
   const [startDate, setStartDate] = useState<Date>(new Date());
+  const [show , setShow] = useState<boolean>(false);
 
   const handleDateChange = (date: Date | null) => {
     if (date) {
@@ -13,11 +22,86 @@ const ViewAttendanse: React.FC = () => {
   };
 
   const handleViewAttendance = () => {
-    // Implement your logic to view attendance based on the selected date
+    setShow(!show)
   };
 
+  // Dummy array of attendance data
+  const dummyData: Attendance[] = [
+    {
+      id: 1,
+      userName: "Puskar",
+      status: "Absent",
+      email: "fake@gmail.com",
+      date: "10/05/28",
+    },
+    {
+      id: 2,
+      userName: "John Doe",
+      status: "Present",
+      email: "john.doe@example.com",
+      date: "10/05/28",
+    },
+    {
+      id: 3,
+      userName: "Jane Smith",
+      status: "Absent",
+      email: "jane.smith@example.com",
+      date: "10/05/28",
+    },
+    {
+      id: 3,
+      userName: "Jane Smith",
+      status: "Absent",
+      email: "jane.smith@example.com",
+      date: "10/05/28",
+    },
+    {
+      id: 3,
+      userName: "Jane Smith",
+      status: "Absent",
+      email: "jane.smith@example.com",
+      date: "10/05/28",
+    },
+    {
+      id: 3,
+      userName: "Jane Smith",
+      status: "Absent",
+      email: "jane.smith@example.com",
+      date: "10/05/28",
+    },
+    {
+      id: 3,
+      userName: "Jane Smith",
+      status: "Absent",
+      email: "jane.smith@example.com",
+      date: "10/05/28",
+    },
+    {
+      id: 3,
+      userName: "Jane Smith",
+      status: "Absent",
+      email: "jane.smith@example.com",
+      date: "10/05/28",
+    },
+    {
+      id: 3,
+      userName: "Jane Smith",
+      status: "Absent",
+      email: "jane.smith@example.com",
+      date: "10/05/28",
+    },
+    {
+      id: 3,
+      userName: "Jane Smith",
+      status: "Absent",
+      email: "jane.smith@example.com",
+      date: "10/05/28",
+    },
+    
+  ];
+
   return (
-    <div className="h-[70vh] w-[80%] mx-auto">
+    <div className="min-h-[70vh]  w-[80%] mx-auto">
       <div className="flex justify-center items-center flex-col gap-8">
         <h1 className="text-2xl xl:text-3xl font-extrabold text-indigo-400 ubuntu-bold mt-12">
           View Attendance
@@ -39,6 +123,43 @@ const ViewAttendanse: React.FC = () => {
           </button>
         </div>
       </div>
+      {show && (<div className="mt-[40px]">
+        <div className="relative overflow-x-auto max-h-[400px]">
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 ">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  User Name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Status
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Email
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Date
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {dummyData.map((attendance) => (
+                <tr key={attendance.id} className="bg-white border-b ">
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+                  >
+                    {attendance.userName}
+                  </th>
+                  <td className="px-6 py-4">{attendance.status}</td>
+                  <td className="px-6 py-4">{attendance.email}</td>
+                  <td className="px-6 py-4">{attendance.date}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>)}
     </div>
   );
 };
