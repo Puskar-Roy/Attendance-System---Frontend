de489b9cf8cae32fd942b9bbc28b898afe51fd1c
Puskar-Roy <puskarroy600@gmail.com>
2024-03-21 02:36:09 +0530

    feat : view attendence

--- src/components/ViewAttendanse.tsx
+++ src/components/ViewAttendanse.tsx
@@ -18,13 +18,25 @@ const ViewAttendanse: React.FC = () => {
 
   return (
     <div className="h-[70vh] w-[80%] mx-auto">
-      <div>
-        <div className="w-[60%] mx-auto flex justify-between">
-          <div>
-            <h1>Select Date</h1>
-            <DatePicker selected={startDate} onChange={handleDateChange} />
+      <div className="flex justify-center items-center flex-col gap-8">
+        <h1 className="text-2xl xl:text-3xl font-extrabold text-indigo-400 ubuntu-bold mt-12">
+          View Attendance
+        </h1>
+        <div className="w-[60%] mx-auto flex flex-col gap-9 sm:gap-0 sm:flex-row justify-between items-center">
+          <div className="flex gap-2 flex-col">
+            <h1 className="ubuntu-medium text-lg font-bold">Select Date</h1>
+            <DatePicker
+              className="border-2 border-gray-600 w-[200px] rounded-md text-center p-1"
+              selected={startDate}
+              onChange={handleDateChange}
+            />
           </div>
-          <button onClick={handleViewAttendance}>View Attendance</button>
+          <button
+            className="h-[50px] w-[160px] bg-indigo-600 text-white rounded-lg"
+            onClick={handleViewAttendance}
+          >
+            View Attendance
+          </button>
         </div>
       </div>
     </div>
