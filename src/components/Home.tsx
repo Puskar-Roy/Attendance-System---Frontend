03706f586b0178f609ea072353c07e397b0d34bc
Puskar-Roy <puskarroy600@gmail.com>
2024-03-20 23:13:14 +0530

    feat : home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,12 +5,12 @@ const Home = () => {
       <div className="flex flex-col gap-[50px]">
         <div className="absolute circlePosition w-screen sm:w-[590px] h-[400px] bg-gradient-to-r from-indigo-400 rounded-[100%] top-[50%] left-[50%]  blur-[90px] translate-x-[-50%] translate-y-[-50%] z-[-1]" />
         <div>
-          <h1 className="text-2xl font-bold ">
+          <h1 className="text-2xl font-bold text-center">
             Your <span className="text-indigo-500">Attendence</span>
           </h1>
         </div>
         <div className="flex flex-col gap-5 justify-center">
-          <div className="flex justify-center  gap-6">
+          <div className="flex justify-center  w-[86%] mx-auto  gap-6">
             <div>
               <h1 className="font-bold">Total Present</h1>
               <div className="border-2  border-green-500 h-[100px] w-[170px] shadow-lg rounded-lg flex justify-center items-center text-black mt-2">
